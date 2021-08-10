4853020551a930e9066966e64a90520ad8dafed2
Gcarvalhu <gustavo190371@gmail.com>
2021-08-10 12:47:51 -0300

    code

---
+++ ForLinguagemC/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 20 //definindo que toda vez que algo estiver representado por MAX terá como valor 5
+
+/* Escrever na tela 5 vezes */
+
+int main() {
+	
+	char c;
+	for(c='A'; c<='z'; c++){
+		
+		printf("%c \t", c);
+	}
+	
+	
+	
+	
+	return 0;
+}
